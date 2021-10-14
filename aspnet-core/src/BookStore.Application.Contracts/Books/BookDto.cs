13b5f859badf416f1ec3dc66dd0b8247ee4424f0
Ali yaser <Ali.yaser.web@gmail.com>
2021-10-14 07:42:34 +0200

    first commit

---
+++ aspnet-core/src/BookStore.Application.Contracts/Books/BookDto.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using Volo.Abp.Application.Dtos;
+
+namespace BookStore.Books
+{
+    public class BookDto : AuditedEntityDto<Guid>
+    {
+
+        public string Name { get; set; }
+
+        public BookType Type { get; set; }
+
+        public DateTime PublishDate { get; set; }
+
+        public float Price { get; set; }
+
+    }
+}
---
+++ aspnet-core/src/BookStore.Domain/Books/Book.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Volo.Abp;
+using Volo.Abp.Domain.Entities.Auditing;
+
+namespace BookStore.Books
+{
+
+
+    public class Book : AuditedAggregateRoot<Guid>
+    {
+
+        public string Name { get; set; }
+
+        public BookType Type { get; set; }
+
+        public DateTime PublishDate { get; set; }
+
+        public float Price { get; set; }
+
+
+
+
+
+    }
+
+
+}
